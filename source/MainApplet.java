21297468150b1c5bcad0e2be2f680c16773781de
VaibhavDomadia <44730103+VaibhavDomadia@users.noreply.github.com>
2018-11-06 10:51:29 +0530

    Update MainApplet.java

--- source/MainApplet.java
+++ source/MainApplet.java
@@ -1,7 +1,9 @@
 package tictactoe;
 
 import java.applet.Applet;
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Color;
 import java.awt.event.*;
 
 /**
@@ -17,16 +19,18 @@ public class MainApplet extends Applet {
       boolean isGameOn = true;
 
       //This shows which blocks from the 9 blocks formed by 3x3 matrix are filled.
-      boolean isTicked[] = new boolean[9];
+      boolean isTicked[][] = new boolean[3][3];
 
       //Giving Random and Distinct values to block other than 0 and 1.
       //if the block is filled with 'X' sign then it will assign 1 to that block, else it will assign 0.
-      int shape[] = {2,3,4,5,6,7,8,9,10};
+      int shape[][] = {{2,3,4},{5,6,7},{8,9,10}};
 
       public void init() {
+            setBackground(Color.lightGray);
+            setForeground(Color.BLACK);
             addMouseListener(new Mouse(this));
             this.setSize(700,800);
-            game = new GameLogic(this,turn);
+            game = new GameLogic(this);
       }
 
       public void update(Graphics g) {
@@ -39,15 +43,17 @@ public class MainApplet extends Applet {
             g.drawLine(400,300,400,600);
             g.drawLine(200,400,500,400);
             g.drawLine(200,500,500,500);
-            for(int i=0 ; i<9 ; i++) {
-                  if(isTicked[i]) {
-                        //update the applet with respective symbol.
-                        if(shape[i] == 1) {
-                              g.drawLine(200+(i%3)*100+25,300+(i/3)*100+25,200+(i%3)*100+75,300+(i/3)*100+75);
-                              g.drawLine(200+(i%3)*100+75,300+(i/3)*100+25,200+(i%3)*100+25,300+(i/3)*100+75);
-                        }
-                        else {
-                              g.drawOval(200+(i%3)*100+25,300+(i/3)*100+25,50,50);
+            for(int i=0 ; i<3 ; i++) {
+                  for(int j=0 ; j<3 ; j++) {
+                        if(isTicked[i][j]) {
+                              //update the applet with respective symbol.
+                              if(shape[i][j] == 1) {
+                                    g.drawLine(200+j*100+25,300+i*100+25,200+j*100+75,300+i*100+75);
+                                    g.drawLine(200+j*100+75,300+i*100+25,200+j*100+25,300+i*100+75);
+                              }
+                              else {
+                                    g.drawOval(200+j*100+25,300+i*100+25,50,50);
+                              }
                         }
                   }
             }
@@ -66,43 +72,42 @@ public class MainApplet extends Applet {
 }
 
 class Mouse extends MouseAdapter {
-      MainApplet m;
-      int x,y;
+      private MainApplet m;
       Mouse(MainApplet m) {
             this.m = m;
       }
       @Override
       public void mouseClicked(MouseEvent e) {
-            x = e.getX();
-            y = e.getY();
+            int x = e.getX();
+            int y = e.getY();
             if(m.isGameOn && m.turn<=9 && x>200 && x<500 && y>300 && y<600) {
                   //First Row
                   if(y>300 && y<=400) {
                         //First Column
-                        if(x>200 && x<=300 && !m.isTicked[0]) {
-                              m.isTicked[0] = true;
+                        if(x>200 && x<=300 && !m.isTicked[0][0]) {
+                              m.isTicked[0][0] = true;
                               if(m.turn%2==1)
-                                    m.shape[0] = 1;
+                                    m.shape[0][0] = 1;
                               else
-                                    m.shape[0] = 0;
+                                    m.shape[0][0] = 0;
                               m.turn++;
                         }
                         //Second Column
-                        else if(x>300 && x<=400 && !m.isTicked[1]) {
-                              m.isTicked[1] = true;
+                        else if(x>300 && x<=400 && !m.isTicked[0][1]) {
+                              m.isTicked[0][1] = true;
                               if(m.turn%2==1)
-                                    m.shape[1] = 1;
+                                    m.shape[0][1] = 1;
                               else
-                                    m.shape[1] = 0;
+                                    m.shape[0][1] = 0;
                               m.turn++;
                         }
                         //Third Column
-                        else if(x>400 && x<=500 && !m.isTicked[2]) {
-                              m.isTicked[2] = true;
+                        else if(x>400 && x<=500 && !m.isTicked[0][2]) {
+                              m.isTicked[0][2] = true;
                               if(m.turn%2==1)
-                                    m.shape[2] = 1;
+                                    m.shape[0][2] = 1;
                               else
-                                    m.shape[2] = 0;
+                                    m.shape[0][2] = 0;
                               m.turn++;
                         }
                   }
@@ -110,30 +115,30 @@ class Mouse extends MouseAdapter {
                   //Second Row
                   else if(y>400 && y<=500) {
                         //First Column
-                        if(x>200 && x<=300 && !m.isTicked[3]) {
-                              m.isTicked[3] = true;
+                        if(x>200 && x<=300 && !m.isTicked[1][0]) {
+                              m.isTicked[1][0] = true;
                               if(m.turn%2==1)
-                                    m.shape[3] = 1;
+                                    m.shape[1][0] = 1;
                               else
-                                    m.shape[3] = 0;
+                                    m.shape[1][0] = 0;
                               m.turn++;
                         }
                         //Second Column
-                        else if(x>300 && x<=400  && !m.isTicked[4]) {
-                              m.isTicked[4] = true;
+                        else if(x>300 && x<=400  && !m.isTicked[1][1]) {
+                              m.isTicked[1][1] = true;
                               if(m.turn%2==1)
-                                    m.shape[4] = 1;
+                                    m.shape[1][1] = 1;
                               else
-                                    m.shape[4] = 0;
+                                    m.shape[1][1] = 0;
                               m.turn++;
                         }
                         //Third Column
-                        else if(x>400 && x<=500 && !m.isTicked[5]) {
-                              m.isTicked[5] = true;
+                        else if(x>400 && x<=500 && !m.isTicked[1][2]) {
+                              m.isTicked[1][2] = true;
                               if(m.turn%2==1)
-                                    m.shape[5] = 1;
+                                    m.shape[1][2] = 1;
                               else
-                                    m.shape[5] = 0;
+                                    m.shape[1][2] = 0;
                               m.turn++;
                         }
                   }
@@ -141,30 +146,30 @@ class Mouse extends MouseAdapter {
                   //Third Row
                   else {
                         //First Column
-                        if(x>200 && x<=300 && !m.isTicked[6]) {
-                              m.isTicked[6] = true;
+                        if(x>200 && x<=300 && !m.isTicked[2][0]) {
+                              m.isTicked[2][0] = true;
                               if(m.turn%2==1)
-                                    m.shape[6] = 1;
+                                    m.shape[2][0] = 1;
                               else
-                                    m.shape[6] = 0;
+                                    m.shape[2][0] = 0;
                               m.turn++;
                         }
                         //Second Column
-                        else if(x>300 && x<=400 && !m.isTicked[7]) {
-                              m.isTicked[7] = true;
+                        else if(x>300 && x<=400 && !m.isTicked[2][1]) {
+                              m.isTicked[2][1] = true;
                               if(m.turn%2==1)
-                                    m.shape[7] = 1;
+                                    m.shape[2][1] = 1;
                               else
-                                    m.shape[7] = 0;
+                                    m.shape[2][1] = 0;
                               m.turn++;
                         }
                         //Third Column
-                        else if(x>400 && x<=500 && !m.isTicked[8]) {
-                              m.isTicked[8] = true;
+                        else if(x>400 && x<=500 && !m.isTicked[2][2]) {
+                              m.isTicked[2][2] = true;
                               if(m.turn%2==1)
-                                    m.shape[8] = 1;
+                                    m.shape[2][2] = 1;
                               else
-                                    m.shape[8] = 0;
+                                    m.shape[2][2] = 0;
                               m.turn++;
                         }
                   }
